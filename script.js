b8152d21910c2d99a51806d426c08d1f72ec1470
addison-lab <146665910+addison-lab@users.noreply.github.com>
2024-03-22 12:46:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ navbarHideBtn.addEventListener('click', function(){
     navbarCollapseDiv.classList.remove('navbar-show')
 });
 
+// Scroll Animate
+window.addEventListener('scroll', reveal);
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
 
 //search icon change on window resize
 window.addEventListener('resize', changeSerachIcon);
